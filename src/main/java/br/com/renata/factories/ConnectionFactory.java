f44c07bbd787f84787627b774ad2b354abc5a6a1
nunesrenata <renatavnpereira@outlook.com>
2026-06-22 01:51:33 -0300

    Fix: Atualizando ConnectionFactory para usar as variáveis do Railway

--- src/main/java/br/com/renata/factories/ConnectionFactory.java
+++ src/main/java/br/com/renata/factories/ConnectionFactory.java
@@ -6,13 +6,30 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 
 	private static final String DRIVER = "org.postgresql.Driver";
-	private static final String HOST = "jdbc:postgresql://localhost:5432/bd_divulga_mais";
-	private static final String USER = "postgres";
-	private static final String PASS = "coti";
 
 	public static Connection getConnection() throws Exception {
-		Class.forName(DRIVER);
-		return DriverManager.getConnection(HOST, USER, PASS);
+		
+		String host = System.getenv("PGHOST");
+		String port = System.getenv("PGPORT");
+		String dbName = System.getenv("PGDATABASE");
+		String user = System.getenv("PGUSER");
+		String password = System.getenv("PGPASSWORD");
 
+		String finalUrl;
+		String finalUser;
+		String finalPass;
+
+		if (host != null && !host.trim().isEmpty()) {
+			finalUrl = "jdbc:postgresql://" + host + ":" + port + "/" + dbName;
+			finalUser = user;
+			finalPass = password;
+		} else {
+			finalUrl = "jdbc:postgresql://localhost:5432/bd_divulga_mais";
+			finalUser = "postgres";
+			finalPass = "coti";
+		}
+
+		Class.forName(DRIVER);
+		return DriverManager.getConnection(finalUrl, finalUser, finalPass);
 	}
 }
